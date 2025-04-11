7f21aa497ab063ffee2f26417c8f033427c1256a
Jamie Lee <126450738+eejl773@users.noreply.github.com>
2025-04-12 09:53:23 +1200

    Set up basic backend

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "Hello from Backend!" });
+});
+
+app.listen(3000, () => console.log("Backend running on http://localhost:3000"));
